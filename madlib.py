302c05f7a9c98b76785676264839ae6776353c81
nizami1878 <nizami1878@gmail.com>
2024-08-14 16:54:07 +0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,7 @@
+adj=input("Adjective:")
+noun1=input("Football club:")
+noun2=input("Football club:")
+noun3=input("Position:")
+madlib=f"Manchester United is the {adj}  football club in England.They are stronger than {noun1} and {noun2}.\n They will be {noun3} this season."
+print(madlib)
+
